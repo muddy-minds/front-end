b781c525ce2b41df15040a2ce8c3871ddbc97853
Taylor <wwwaaavvveeerrr@gmail.com>
2019-07-30 11:57:19 -0400

    axios connected to heroku

--- client/src/comp/Data.js
+++ client/src/comp/Data.js
@@ -2,33 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default ({ url }) => {
-  const [data, setData] = useState(null);
-  useEffect(
-    () => {
-      let mounted = true;
-
-      const loadData = async () => {
-        const { data } = await axios.get('heroku');
-        console.log("data = ", data, "\n");
-        if (mounted) {
-          setData(data);
-        }
-      };
-
-      loadData();
-
-      return () => {
-        mounted = false;
-      };
-    },
-    [url]
-  );
-
-  if (!data) {
-    return <span data-testid="loading"> Loading data...</span>;
-  }
-
-  console.log("data = ", data, "\n");
-
-  return <h1>{data}</h1>;
+    const [datatype, setDatatype] = useState("");
+    useEffect(() => {
+      axios
+        .get("https://muddyminds.herokuapp.com/")
+        .then(res => setDatatype(res.data.message), 
+        console.log('made it!'))
+        .catch(err => console.log(err));
+    }, []);
+  
+    return (
+      <div className="App">
+        <h1>{datatype}</h1>
+      </div>
+    );
 };
