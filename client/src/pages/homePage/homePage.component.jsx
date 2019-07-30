360c079caed7e7f5a7d037fba07b2fe9633090ba
guillsav <guillsav@gmail.com>
2019-07-30 11:40:30 -0400

    fixed className warning

--- client/src/pages/homePage/homePage.component.jsx
+++ client/src/pages/homePage/homePage.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './homePage.styles.scss';
 
 const HomePage = () => {
-    return <div class="homepage">HomePage</div>;
+    return <div className="homepage">HomePage</div>;
 };
 
 export default HomePage;
