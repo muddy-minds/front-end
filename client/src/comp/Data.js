594ca12b5d04ba104abf2fcd21e561aea1ecba03
Taylor <wwwaaavvveeerrr@gmail.com>
2019-07-31 15:48:06 -0400

    created boiler plate componenets to interact with the Game Display, Use data.js to refence how to write useeffect and play with the server data

--- client/src/comp/Data.js
+++ client/src/comp/Data.js
@@ -2,18 +2,17 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default ({ url }) => {
-    const [datatype, setDatatype] = useState("");
+    const [datatype, setDatatype] = useState([]);
     useEffect(() => {
       axios
-        .get("https://muddyminds.herokuapp.com/")
-        .then(res => setDatatype(res.data.message), 
-        console.log('made it!'))
-        .catch(err => console.log(err));
+        .get("https://muddyminds.herokuapp.com/api/rooms/")
+        .then(response => setDatatype(response.data))
+        .catch(err => console.log("an error occurs", err));
     }, []);
   
     return (
-      <div className="App">
-        <h1>{datatype}</h1>
+      <div>
+      {datatype.map(data => <h2>{data.name}</h2>)}
       </div>
     );
 };
